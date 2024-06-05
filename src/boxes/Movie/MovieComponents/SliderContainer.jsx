cb1a9261e7469dc228b19e32d09b9f652679357c
ashiii1 <aashishaik526@gmail.com>
2024-06-05 19:42:29 +0530

    slider error resolved

--- src/boxes/Movie/MovieComponents/SliderContainer.jsx
+++ src/boxes/Movie/MovieComponents/SliderContainer.jsx
@@ -1,54 +1,53 @@
-// import React from 'react'
-// import styled from 'styled-components'
-// import MovieSlider from './MovieSlider'
+import React from 'react'
+import styled from 'styled-components'
+import MovieSlider from './MovieSlider'
 
-// const SliderContainer = ({movies}) => {
+const SliderContainer = ({movies}) => {
 
-//     const getMoviesBetween = (start, end)=>{
-//         return movies.slice(start, end)
-//     }
-
-//   return (
-//    <SliderWrapper>
-//       <MovieSlider data={getMoviesBetween(0,10)} title="Only On Stream" />
-//       <MovieSlider data={getMoviesBetween(10,20)} title="Trending now" />
-//       <MovieSlider data={getMoviesBetween(20,30)} title="Popular On Stream " />
-//       <MovieSlider data={getMoviesBetween(30,40)} title="Romantic Movie" />
-//       <MovieSlider data={getMoviesBetween(40,50)} title="Epic" />
-//       <MovieSlider data={getMoviesBetween(50,60)} title="New Releases" />
-//       <MovieSlider data={getMoviesBetween(60,70)} title="Action Movies" />
-//    </SliderWrapper>
-//   )
-// }
-// const SliderWrapper = styled.div`
-
-// }
-// `
-
-// export default SliderContainer
-// SliderContainer.jsx
-import React from 'react';
-import styled from 'styled-components';
-import MovieSlider from './MovieSlider';
-
-const SliderContainer = ({ movies }) => {
-  const getMoviesBetween = (start, end) => {
-    return movies.slice(start, end);
-  };
+    const getMoviesBetween = (start, end)=>{
+        return movies.slice(start, end)
+    }
 
   return (
-    <SliderWrapper>
-      <MovieSlider data={getMoviesBetween(0, 10)} title="Only On Stream" />
-      <MovieSlider data={getMoviesBetween(10, 20)} title="Trending now" />
-      <MovieSlider data={getMoviesBetween(20, 30)} title="Popular On Stream" />
-      <MovieSlider data={getMoviesBetween(30, 40)} title="Romantic Movie" />
-      <MovieSlider data={getMoviesBetween(40, 50)} title="Epic" />
-      <MovieSlider data={getMoviesBetween(50, 60)} title="New Releases" />
-      <MovieSlider data={getMoviesBetween(60, 70)} title="Action Movies" />
-    </SliderWrapper>
-  );
-};
+   <SliderWrapper>
+      <MovieSlider data={getMoviesBetween(0,10)} title="Only On Stream" />
+      <MovieSlider data={getMoviesBetween(10,20)} title="Trending now" />
+      <MovieSlider data={getMoviesBetween(20,30)} title="Popular On Stream " />
+      <MovieSlider data={getMoviesBetween(30,40)} title="Romantic Movie" />
+      <MovieSlider data={getMoviesBetween(40,50)} title="Epic" />
+      <MovieSlider data={getMoviesBetween(50,60)} title="New Releases" />
+      <MovieSlider data={getMoviesBetween(60,70)} title="Action Movies" />
+   </SliderWrapper>
+  )
+}
+const SliderWrapper = styled.div`
 
-const SliderWrapper = styled.div``;
+}
+`
 
-export default SliderContainer;
+export default SliderContainer
+// import React from 'react';
+// import styled from 'styled-components';
+// import MovieSlider from './MovieSlider';
+
+// const SliderContainer = ({ movies }) => {
+//   const getMoviesBetween = (start, end) => {
+//     return movies.slice(start, end);
+//   };
+
+//   return (
+//     <SliderWrapper>
+//       <MovieSlider data={getMoviesBetween(0, 10)} title="Only On Stream" />
+//       <MovieSlider data={getMoviesBetween(10, 20)} title="Trending now" />
+//       <MovieSlider data={getMoviesBetween(20, 30)} title="Popular On Stream" />
+//       <MovieSlider data={getMoviesBetween(30, 40)} title="Romantic Movie" />
+//       <MovieSlider data={getMoviesBetween(40, 50)} title="Epic" />
+//       <MovieSlider data={getMoviesBetween(50, 60)} title="New Releases" />
+//       <MovieSlider data={getMoviesBetween(60, 70)} title="Action Movies" />
+//     </SliderWrapper>
+//   );
+// };
+
+// const SliderWrapper = styled.div``;
+
+// export default SliderContainer;
